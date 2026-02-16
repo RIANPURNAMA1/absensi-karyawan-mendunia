df135e8c90cf8942a8af60bb1f3bcdf805ba420d
Rian Purnama <emailgithubkamu@gmail.com>
2026-02-16 20:34:20 +0700

    perubahan

--- routes/web.php
+++ routes/web.php
@@ -139,8 +139,12 @@ Route::middleware(['auth', 'role:HR,MANAGER,KARYAWAN'])->group(function () {
 |--------------------------------------------------------------------------
 */
 
-Route::get('/absensi', [AbsensiController::class, 'index'])->name('absensi.index');
+
+Route::get('/test-akses', function() {
+    return "Akses Berhasil!";
+});
 Route::middleware(['auth', 'role:KARYAWAN'])->group(function () {
+    Route::get('/absensi', [AbsensiController::class, 'index'])->name('absensi.index');
 
     // absensi foto
     Route::post('/absensi/foto/proses', [AbsensiController::class, 'absenFoto'])->middleware('auth');
